3db2c78f471b90e3d0457dfc013dbdcee3765892
Ajay-Kumar-Saroj <118628778+Ajay-Kumar-Saroj@users.noreply.github.com>
2026-02-01 15:57:37 +0530

    Fix formatting and syntax in script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const text = "I made something special just for you...";\
-let index = 0;\
-const messageEl = document.getElementById("message");\
-\
-function typeEffect() \{\
-  if (index < text.length) \{\
-    messageEl.innerHTML += text.charAt(index);\
-    index++;\
-    setTimeout(typeEffect, 80);\
-  \}\
-\}\
-\
-typeEffect();\
-\
-document.getElementById("revealBtn").addEventListener("click", () => \{\
-  document.getElementById("surprise").classList.remove("hidden");\
-\});}
+const text = "I made something special just for you...";
+let index = 0;
+const messageEl = document.getElementById("message");
+
+function typeEffect() {
+  if (index < text.length) {
+    messageEl.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 80);
+  }
+}
+
+typeEffect();
+
+document.getElementById("revealBtn").addEventListener("click", () => {
+  document.getElementById("surprise").classList.remove("hidden");
+});
+
